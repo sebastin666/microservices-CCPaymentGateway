287c737893a06a6986ad22793790d7a6811ed6ca
SEBUJAS <SEBUJAS@SEBU>
2019-08-02 12:55:02 +0530

    1st Commit

---
+++ CCPaymentGateway/src/main/java/com/itc/trn/mis/CcPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.itc.trn.mis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcPaymentGatewayApplication.class, args);
+	}
+
+}
